a60acd5637258b813f59cd0c023c8255c1ec4442
remnanbenj <ben.remnant@gmail.com>
2018-05-14 18:06:01 +1200

    Added channels to power useage

--- routes/device.js
+++ routes/device.js
@@ -60,7 +60,8 @@ function renderAMP(req, res, device, data, timescale){
   if(req.query.channels) channels = req.query.channels;
 
   for(var i = 0; i < data.length; i++) {
-    data[i].reading = [data[i].receivedtime];
+    if(startDate.getTimezoneOffset() != -720) data[i].reading = [(new Date(data[i].receivedtime)).setHours((new Date(data[i].receivedtime)).getHours() - 4)];
+    else data[i].reading = [data[i].receivedtime];
     if(channels.indexOf('1') != -1) data[i].reading.push(Number(data[i].data.split(':')[0]) * 230);
     if(channels.indexOf('2') != -1) data[i].reading.push(Number(data[i].data.split(':')[1]) * 230);
     if(channels.indexOf('3') != -1) data[i].reading.push(Number(data[i].data.split(':')[2]) * 230);
