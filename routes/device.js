777aa68b7a4580c9e7433052eec9640b3c339860
remnanbenj <ben.remnant@gmail.com>
2018-05-16 18:02:09 +1200

    quick fix

--- routes/device.js
+++ routes/device.js
@@ -437,7 +437,7 @@ router.get('/getdata', checkSignIn, function(req, res) {
         dataRow = [];
         dataRow.push(new Date(results[i].receivedtime));
         for(var j = 0; j < channels.length; j++){
-          dataRow.push(readings[channels[j-1]]*230);
+          dataRow.push(readings[Number(channels[j])]*230);
         }
         data.push(dataRow);
       }
