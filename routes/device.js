acd628faf371b839a74517b513516b9dc26fa37a
remnanbenj <ben.remnant@gmail.com>
2018-05-21 11:20:48 +1200

    fixed clear bug

--- routes/device.js
+++ routes/device.js
@@ -125,7 +125,7 @@ router.get('/getdata', checkSignIn, function(req, res) {
 
     } else if(type == "WTRLVL") {
       dataRow.push('Time');
-      dataRow.push('Metre');
+      dataRow.push('Metres');
     }
     data.push(dataRow);
 
