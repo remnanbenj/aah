b70bef0bc000ae1f0865b182a0030c07b60120b3
WebServer <ben.remnant@gmail.com>
2018-10-15 18:44:43 +1300

    adding relay to temp

--- routes/data.js
+++ routes/data.js
@@ -56,6 +56,7 @@ router.get('/reading', function(req, res) {
       if(type == "TEMP") {
 
         var variables = devices[0].variables.split(',');
+        data = data.split(':');
         console.log(variables);
         console.log(data);
 
