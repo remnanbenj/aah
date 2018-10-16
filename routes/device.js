7a53225603ce823259604e5bdab3a99d31233fcc
WebServer <ben.remnant@gmail.com>
2018-10-16 19:54:06 +1300

    adding relay to temp

--- routes/device.js
+++ routes/device.js
@@ -289,7 +289,7 @@ function reduceTempResults(data, timeScale, startDate, endDate) {
 
   if(timeScale == 'day') {
 
-    var minutes = 60;
+    var minutes = 30;
     tEndDate.setMinutes(tEndDate.getMinutes() + minutes);
 
     for(var i = 0; i < diffMins / minutes; i++){
