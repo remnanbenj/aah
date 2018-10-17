53d8eefb0c791dea0ab3c04c26d91c023190f9e5
WebServer <ben.remnant@gmail.com>
2018-10-17 14:36:16 +1300

    adding power to water heater

--- routes/device.js
+++ routes/device.js
@@ -87,7 +87,7 @@ router.get('/getdata', checkSignIn, function(req, res) {
       if(results.length > 0) { // If we have data, put it into an array
         for(var i = 0; i < results.length; i++){
           var tempDate = new Date(results[i].receivedtime);
-          tempDate.setMinutes(tempDate.getMinutes() + 300);
+          tempDate.setMinutes(tempDate.getMinutes() - 300);
           var readings = results[i].data.split(':');
           dataRow = [];
           dataRow.push(tempDate);
@@ -130,7 +130,7 @@ router.get('/getdata', checkSignIn, function(req, res) {
         if(results.length > 0) { // If we have data, put it into an array
           for(var i = 0; i < results.length; i++){
             var tempDate = new Date(results[i].receivedtime);
-            tempDate.setMinutes(tempDate.getMinutes() + 300);
+            tempDate.setMinutes(tempDate.getMinutes() - 300);
             dataRow = [];
             dataRow.push(tempDate);
             dataRow.push(results[i].data.split(':')[0]);
