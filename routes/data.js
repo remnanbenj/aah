740730752170af1963f3829d44f605938d0a7634
WebServer <ben.remnant@gmail.com>
2018-10-24 16:09:31 +1300

    Creating home page

--- routes/data.js
+++ routes/data.js
@@ -48,7 +48,7 @@ router.get('/reading', function(req, res) {
 
     // Else: Update last reading and send data to device
     } else {
-      var sql = "update devices set lastreading = '"+receivedtime+"' and lastreadingdata = '"+data+"' where mac = '"+mac+"';";
+      var sql = "update devices set lastreading = '"+receivedtime+"', lastreadingdata = '"+data+"' where mac = '"+mac+"';";
       console.log(sql);
       con.query(sql, function (err) { if (err) throw err; });
 
