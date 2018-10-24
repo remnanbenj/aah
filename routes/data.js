d170b8b7c13f78aa58609849a66ea4099da3fc79
WebServer <ben.remnant@gmail.com>
2018-10-24 16:27:42 +1300

    Creating home page

--- routes/data.js
+++ routes/data.js
@@ -49,7 +49,6 @@ router.get('/reading', function(req, res) {
     // Else: Update last reading and send data to device
     } else {
       var sql = "update devices set lastreading = '"+receivedtime+"', lastreadingdata = '"+data+"' where mac = '"+mac+"';";
-      console.log(sql);
       con.query(sql, function (err) { if (err) throw err; });
 
       // ===POWER MONITOR===
